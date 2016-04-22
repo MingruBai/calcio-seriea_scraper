38e859d3fbbd0a851a7520c8823b6cc303f76b4c
Victor Bai <mingru.bai@princeton.edu>
2016-04-21 21:20:35 -0400

    working on each game page

--- scrape.py
+++ scrape.py
@@ -3,22 +3,51 @@ import bs4
 import re
 import time
 
+#Page for a season:
 response = requests.get('http://calcio-seriea.net/risultati/2014');
 soup = bs4.BeautifulSoup(response.text, "html.parser");
-# print response
-# print soup
 tables = soup.find_all('table');
+#Table for each fixture:
 fixtures = tables[8];
-
-# print tables
 rows = fixtures.find_all('tr');
-
 dates = rows[0].find_all('a') + rows[1].find_all('a');
 
+#For each fixture:
 for date in dates:
+	#Get fixture number:
+	fixture = date.text.strip();
 	dateURL = "http://calcio-seriea.net" + date['href'];
-	print dateURL;
-	
+	#Page for each date:
+	response = requests.get(dateURL);
+	soup = bs4.BeautifulSoup(response.text, "html.parser");
+
+	tables = soup.find_all('table');
+	results = tables[11];
+	games = results.find_all('tr');
+
+	#For each game:
+	for game in games:
+		objects = game.find_all('a');
+		if len(objects) == 0: continue;
+		homeTeam = objects[0].text.strip();
+		awayTeam = objects[1].text.strip();
+		gameResult = objects[2].text.strip();
+		gameURL = "http://calcio-seriea.net" + objects[2]['href'];
+
+		
+		#Page for each game:
+		response = requests.get(gameURL);
+		soup = bs4.BeautifulSoup(response.text, "html.parser");
+		tables = soup.find_all('table');
+
+		gameStats = tables[8];
+		rows = gameStats.find_all('tr');
+
+		gameStats.find_all('tr')[14].find_all('td')[0]
+
+
+
+
 
 
 
